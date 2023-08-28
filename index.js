972f9205a8114b6591138deda66c1d1b7105e026
Vinidaily <arthur@vinidaily.com>
2023-08-28 18:48:41 +0200

    Parametrize env and debug the code to run it\n\nThis commit adds debugging to the original code and parametrization of the environment, it includes:\n  - Allow adds a rule for the eslint parser to allow parentheses spacing\n  - Solve WriteFile function is not taking string[] but string to write clean logs in the output.txt

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const discountOffers = [
   new DiscountOffer("Velib", 20, 30),
   new DiscountOffer("Naturalia", 10, 5),
   new DiscountOffer("Vinted", 5, 40),
-  new DiscountOffer("Ilek", 15, 40)
+  new DiscountOffer("Ilek", 15, 40),
 ];
 const store = new Store(discountOffers);
 
@@ -17,7 +17,8 @@ for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
 }
 
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log, err => {
+//Here we are iterated on the log as writeFile functions only take the type string as arguments
+fs.writeFile("output.txt", log.join("\n"), (err) => {
   if (err) {
     console.log("error");
   } else {
--- store.js
+++ store.js
@@ -18,21 +18,25 @@ export class Store {
       ) {
         if (this.discountOffers[i].discountInPercent > 0) {
           if (this.discountOffers[i].partnerName != "Ilek") {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
+            this.discountOffers[i].discountInPercent =
+              this.discountOffers[i].discountInPercent - 1;
           }
         }
       } else {
         if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
+          this.discountOffers[i].discountInPercent =
+            this.discountOffers[i].discountInPercent + 1;
           if (this.discountOffers[i].partnerName == "Vinted") {
             if (this.discountOffers[i].expiresIn < 11) {
               if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
+                this.discountOffers[i].discountInPercent =
+                  this.discountOffers[i].discountInPercent + 1;
               }
             }
             if (this.discountOffers[i].expiresIn < 6) {
               if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
+                this.discountOffers[i].discountInPercent =
+                  this.discountOffers[i].discountInPercent + 1;
               }
             }
           }
@@ -46,21 +50,23 @@ export class Store {
           if (this.discountOffers[i].partnerName != "Vinted") {
             if (this.discountOffers[i].discountInPercent > 0) {
               if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
+                this.discountOffers[i].discountInPercent =
+                  this.discountOffers[i].discountInPercent - 1;
               }
             }
           } else {
             this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - this.discountOffers[i].discountInPercent;
+              this.discountOffers[i].discountInPercent -
+              this.discountOffers[i].discountInPercent;
           }
         } else {
           if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
+            this.discountOffers[i].discountInPercent =
+              this.discountOffers[i].discountInPercent + 1;
           }
         }
       }
     }
-
     return this.discountOffers;
   }
 }
